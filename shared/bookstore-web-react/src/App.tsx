09c5bea945eb2aa579040ae846dc0b04b9cf8795
Taylar <keast.taylar@gmail.com>
2023-01-23 14:01:46 +1100

    Retrieve books

--- shared/bookstore-web-react/src/App.tsx
+++ shared/bookstore-web-react/src/App.tsx
@@ -1,20 +1,14 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import BookSearchPage from "./pages/bookSearch/BookSearchPage";
 import DashboardPage from "./pages/dashboard/DashboardPage";
 
-function App() {
+export default function App() {
   return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<DashboardPage></DashboardPage>} />
-          <Route path="/books" element={<BookSearchPage></BookSearchPage>} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage></DashboardPage>} />
+        <Route path="/books" element={<BookSearchPage></BookSearchPage>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
--- shared/bookstore-web-react/src/pages/bookSearch/BookSearchPage.tsx
+++ shared/bookstore-web-react/src/pages/bookSearch/BookSearchPage.tsx
@@ -1,3 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function BookSearchPage() {
-  return <div>BookSearchPage</div>;
+  const [books, setBooks] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/books")
+      .then((res) => setBooks(res.data.results));
+  }, []);
+
+  return (
+    <div>
+      BookSearchPage
+      <div>
+        {books.map((book) => (
+          <span>{book.title}</span>
+        ))}
+      </div>
+    </div>
+  );
 }
