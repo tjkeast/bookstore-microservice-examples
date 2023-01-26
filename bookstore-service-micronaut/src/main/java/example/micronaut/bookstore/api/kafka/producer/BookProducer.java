07539919ab972f310de4b07ba39c8481da2428bc
Taylar <keast.taylar@gmail.com>
2023-01-26 14:22:34 +1100

    Book Kafka producer

---
+++ bookstore-service-micronaut/src/main/java/example/micronaut/bookstore/api/kafka/producer/BookProducer.java
@@ -0,0 +1,16 @@
+package example.micronaut.bookstore.api.kafka.producer;
+
+import example.micronaut.bookstore.api.model.Book;
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import io.micronaut.configuration.kafka.annotation.KafkaKey;
+import io.micronaut.configuration.kafka.annotation.Topic;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.StringUtils;
+
+@Requires(property = "kafka.enabled", value = StringUtils.TRUE)
+@KafkaClient
+public interface BookProducer {
+
+    @Topic("example.bookstore.topic.book")
+    void sendBook(@KafkaKey String key, Book book);
+}
--- bookstore-service-micronaut/src/main/java/example/micronaut/bookstore/core/service/BookService.java
+++ bookstore-service-micronaut/src/main/java/example/micronaut/bookstore/core/service/BookService.java
@@ -1,17 +1,53 @@
 package example.micronaut.bookstore.core.service;
 
+import example.micronaut.bookstore.api.kafka.producer.BookProducer;
 import example.micronaut.bookstore.api.model.Book;
 import example.micronaut.bookstore.api.model.BookRequest;
 import example.micronaut.bookstore.api.model.search.BookSearchCriteria;
 import example.micronaut.bookstore.api.model.search.PagedSearchResult;
+import example.micronaut.bookstore.core.mapper.EntityMapper;
+import example.micronaut.bookstore.persistence.entity.BookEntity;
+import example.micronaut.bookstore.persistence.repository.BookRepository;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
-public interface BookService {
+import javax.transaction.Transactional;
+import java.util.UUID;
 
-    PagedSearchResult<Book> getBooks(BookSearchCriteria criteria);
+@Singleton
+public class BookService {
 
-    Book getBook(Long bookId);
+    @Inject
+    private BookRepository bookRepository;
 
-    Book createBook(BookRequest request);
+    @Inject
+    private EntityMapper entityMapper;
 
-    Book updateBook(Long bookId, BookRequest request);
+    @Inject
+    private BookProducer bookProducer;
+
+    @Transactional
+    public PagedSearchResult<Book> getBooks(BookSearchCriteria criteria) {
+        return PagedSearchResult.from(bookRepository.findAll(criteria, criteria.toPageable()), entityMapper::map);
+    }
+
+    public Book getBook(Long bookId) {
+        return entityMapper.map(bookRepository.findById(bookId)
+                .orElseThrow(() -> new IllegalArgumentException("Book with ID %s does not exist".formatted(bookId))));
+    }
+
+    public Book createBook(BookRequest request) {
+        BookEntity book = entityMapper.map(request);
+        Book newBook = entityMapper.map(bookRepository.save(book));
+        bookProducer.sendBook(UUID.randomUUID().toString(), newBook);
+        return newBook;
+    }
+
+    public Book updateBook(Long bookId, BookRequest request) {
+        BookEntity book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new IllegalArgumentException("Book with ID %s does not exist".formatted(bookId)));
+        entityMapper.update(book, request);
+
+        return entityMapper.map(bookRepository.update(book));
+    }
 }
